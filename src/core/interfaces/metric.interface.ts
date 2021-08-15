a3c4b5c235595da012ef2ea0832d9b6a8576ea73
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-15 16:57:34 +0200

    Adding MetricInterface

--- src/core/interfaces/metric.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface MetricInterface {
-    filesHighThreshold?: number;
-    filesMediumThreshold?: number;
-    methodsHighThreshold: number;
-    methodsMediumThreshold: number;
-    name: string;
-}
