5ecfce3c869fc52b8373a0e54cedcabc5bde9547
Gilles Fabre <gillesfabre34@gmail.com>
2021-08-16 12:32:56 +0200

    Added FunctionInterface

---
+++ src/json-ast-to-ast-model/services/ast-function-interface.service.ts
@@ -0,0 +1,9 @@
+import { AstFile } from '../../core/models/ast/ast-file.model';
+import { FunctionInterface } from '../../core/interfaces/ast/function.interface';
+
+export class AstFunctionInterfaceService {
+
+    static generate(astFile: AstFile): FunctionInterface[] {
+        return [];
+    }
+}
---
+++ src/json-ast-to-ast-model/services/ast-method.service.ts
@@ -0,0 +1,10 @@
+import { AstFile } from '../../core/models/ast/ast-file.model';
+import { AstMethod } from '../../core/models/ast/ast-method.model';
+
+export class AstMethodService {
+
+    static generate(astFile: AstFile): AstMethod[] {
+        return [];
+    }
+
+}
