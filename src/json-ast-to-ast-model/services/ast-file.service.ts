2e3dcd2731b6e5f4f62c16055a0e140c1ffa3b58
Genese <63037808+geneseframework@users.noreply.github.com>
2021-08-20 10:57:52 +0200

    Fixed bug with lines order (#45)
    * Fixed bug with linePositions
    
    * Fixed bug with lines order
    
    Co-authored-by: Gilles Fabre <gillesfabre34@gmail.com>

--- src/json-ast-to-ast-model/services/ast-file.service.ts
+++ src/json-ast-to-ast-model/services/ast-file.service.ts
@@ -16,10 +16,8 @@ export class AstFileService {
         astFile.astFunctions = AstFunctionService.generate(astFile);
         astFile.astArrowFunctions = AstArrowFunctionService.generate(astFile);
         astFile.astCode = AstCodeService.generate(astFile);
-        // console.log(chalk.greenBright('AST CODEEEEEEE lines'), astFile.lines.map(l => l.text));
-        // console.log(chalk.magentaBright('AST CODEEEEEEE code'), astFile.code);
+        // console.log(chalk.magentaBright('AST FILEEEE = '), astFile.astCode.astClassOrFunctionCodes[0]);
         astFile.astCode.logg();
-        // console.log(chalk.magentaBright('AST FILEEEE CODE = '), astFile);
         return astFile;
     }
 }
--- src/json-ast-to-ast-model/services/ast-line.service.ts
+++ src/json-ast-to-ast-model/services/ast-line.service.ts
@@ -1,9 +1,9 @@
 import { AstCode } from '../../core/models/ast/ast-code.model';
 import { AstAbstract } from '../../core/models/ast/ast-abstract.model';
 import { AstLine } from '../../core/models/ast/ast-line.model';
-import * as chalk from 'chalk';
 import { AstNode } from '../../core/models/ast/ast-node.model';
-import { firstElement, lastElement } from '../../core/utils/arrays.util';
+import { firstElement } from '../../core/utils/arrays.util';
+import { Interval } from '../types/interval.type';
 
 export class AstLineService {
 
@@ -16,12 +16,11 @@ export class AstLineService {
         let issue = 1;
         const astLines: AstLine[] = [];
         let position: number = astCode.astAbstract.interval[0];
-        // console.log(chalk.yellowBright('NB LINESSSSSS'), textLines.length);
         for (const textLine of textLines) {
             const line = new AstLine();
             line.text = textLine;
             line.issue = issue;
-            line.pos = position; // TODO : fix for lines after intervals
+            line.pos = this.getLinePos(position, astCode.astAbstract);
             line.end = line.pos + textLine.length + 1;
             line.astNodes = this.getAstNodes(astCode.astAbstract, line.pos, line.end);
             issue++;
@@ -39,6 +38,12 @@ export class AstLineService {
 
     private static getAstNodes(astAbstract: AstAbstract, pos: number, end: number): AstNode[] {
         return astAbstract.astNode.descendants.filter(d => d.pos >= pos && d.pos < end - 1);
+    }
+
+    private static getLinePos(position: number, astAbstract: AstAbstract): number {
+        const posInterval: Interval = astAbstract.positionInterval(position + 1);
+        // console.log(chalk.greenBright('GET LINE POSSSS'), astAbstract.kind, astAbstract.name, astAbstract.classesAndFunctionsIntervals, position, posInterval);
+        return posInterval ? posInterval[1] + 1 : position;
     }
 
 }
