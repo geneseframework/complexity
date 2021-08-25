e64eccac6d13716556153d8ec6bb4f0611cad5de
Gilles FABRE <gillesfabrejob@gmail.com>
2021-08-25 12:21:39 +0200

    Creating repository metrics (#50)

--- src/report-generation/models/html-report.model.ts
+++ src/report-generation/models/html-report.model.ts
@@ -1,7 +1,9 @@
 import { RowSnippet } from './row-snippet.model';
+import { DivCodeMetric } from './div-code-metric.model';
 
 export class HtmlReport {
 
+    divCodeMetrics: DivCodeMetric[] = [];
     rowSnippets: RowSnippet[] = [];
     metricNames: string[] = [];
 
