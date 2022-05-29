1bbca8076073541f8c3d2db0518d54763674fe9f
gillesfabre34 <gillesfabre34@gmail.com>
2022-05-29 10:39:46 +0200

    Fixed bug with core clone

--- src/core/utils/arrays.util.ts
+++ src/core/utils/arrays.util.ts
@@ -30,3 +30,24 @@ export function lastElement<T>(array: T[]): T {
 export function sum(array: number[]): number {
     return Array.isArray(array) ? array.reduce((a, b) => a + b, 0) : undefined;
 }
+
+/**
+ * Returns cloned object with deep copy
+ */
+export function clone(model: any): any {
+    if (model) {
+        if (Array.isArray(model)) {
+            const newArray = [];
+            model.forEach(item => newArray.push(clone(item)));
+            return newArray;
+        } else if (typeof model === 'object') {
+            const newObject = {};
+            Object.keys(model).forEach(key => newObject[key] = clone(model[key]));
+            return newObject;
+        } else {
+            return model;
+        }
+    } else {
+        return model;
+    }
+}
